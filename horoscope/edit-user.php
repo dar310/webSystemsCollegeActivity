07bfcb0c92f1b8e31b3f4d2c4e612b77ef69439e
dar310 <76474601+dar310@users.noreply.github.com>
2024-12-22 17:19:59 +0800

    Updated the dashboard with operations working
    update the dashboard for assurance
    added add,edit, and delete operations

---
+++ horoscope/edit-user.php
@@ -0,0 +1,109 @@
+<?php
+
+include_once("config.php");
+
+// Check if form is submitted for user update, then redirect to homepage after update
+if(isset($_POST['update']))
+{	
+	$id = $_POST['id'];
+	
+	$first_name=$_POST['first_name'];
+	$last_name=$_POST['last_name'];
+	$birthday=$_POST['birthday'];
+	$gender=$_POST['gender'];
+	$is_admin=$_POST['isAdmin'];
+		
+	// update user data
+	$result = mysqli_query($conn, "UPDATE users SET first_name='$first_name',last_name='$last_name', birthday='$birthday',gender='$gender',is_admin='$is_admin' WHERE id=$id");
+	
+	// Redirect to homepage to display updated user in list
+	header("Location: admindashboard.php");
+}
+?>
+<?php
+// Display selected user data based on id
+// Getting id from url
+$id = $_GET['id'];
+
+// Fetech user data based on id
+$result = mysqli_query($conn, "SELECT * FROM users WHERE id=$id");
+
+while($user_data = mysqli_fetch_array($result))
+{
+	$first_name = $user_data['first_name'];
+	$last_name = $user_data['last_name'];
+	$birthday = $user_data['birthday'];
+	$gender = $user_data['gender'];
+	$is_admin = $user_data['is_admin'];
+}
+?>
+<html>
+	<head>	
+		<title>Edit User Data</title>
+		<link rel="stylesheet" href="styles/register.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+        <style>
+            textarea {
+                resize: none;
+            }
+        </style>
+	</head>
+
+	<body>
+		<a href="admindashboard.php" class="btn btn-primary">Home</a>
+		<br/><br/>
+		
+		<form name="update_user" method="post" action="edit-user.php">
+            <div class="mb-3">
+                <table width="100%" border="0">
+                    <tr>
+                        <td><label class="form-label">First Name</label></td>
+                        <td><input type="text" class="form-control" name="first_name" value="<?php echo $first_name;?>" required></td>
+                    </tr>
+                    <tr> 
+                        <td><label class="form-label">Last Name</label></td>
+                        <td><input type="text" class="form-control" name="last_name" value="<?php echo $last_name;?>" required></td>
+                    </tr>
+                    <tr> 
+                        <td><label class="form-label">Birthday</label></td>
+                        <td><input type="date" name="birthday" class="form-control" value=<?php echo $birthday;?> required></td>
+                    </tr>
+                    <tr> 
+                        <td><label class="form-label">Gender</label></td>
+                        <td>
+                            <label>
+                                <input type="radio" name="gender" class="form-check-input" value="male" 
+                                <?php if($gender == 'male') echo 'checked'; ?> required> Male
+                            </label>
+                            <label>
+                                <input type="radio" name="gender" class="form-check-input" value="female" 
+                                <?php if($gender == 'female') echo 'checked'; ?>> Female
+                            </label>
+                            <label>
+                                <input type="radio" name="gender" class="form-check-input" value="other" 
+                                <?php if($gender == 'other') echo 'checked'; ?>> Other
+                            </label>
+                        </td>
+                    </tr>
+                    <tr> 
+                        <td><label class="form-label">Is Administrator?</label></td>
+                        <td>
+                            <label>
+                                <input type="radio" name="isAdmin" class="form-check-input" value="0" <?php if($is_admin == '0') echo 'checked'; ?> required>No
+                            </label>
+                            <label>
+                                <input type="radio" name="isAdmin" class="form-check-input" value="1" <?php if($is_admin == '1') echo 'checked'; ?>>Yes
+                            </label>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td><input type="hidden" name="id" value=<?php echo $_GET['id'];?>></td>
+                        <td><input type="submit" class="btn btn-primary"name="update" value="Update"></td>
+                    </tr>
+                </table>
+            </div>
+		</form>
+	</body>
+</html>
+
---
+++ horoscope/edit-zodiac.php
@@ -0,0 +1,137 @@
+<?php
+    include_once("config.php");
+
+    // Check if form is submitted for user update, then redirect to homepage after update
+    if(isset($_POST['update']))
+    {	
+        $id = $_POST['id'];
+        
+        $sign_name = $_POST['sign_name'];
+        $description = $_POST['description'];
+        $daily_horoscope = $_POST['daily_horoscope'];
+        $min_month = $_POST['min_month'];
+        $min_day = $_POST['min_day'];
+        $max_month = $_POST['max_month'];
+        $max_day = $_POST['max_day'];
+        $image_path = $_POST['image_path'];
+            
+        // update user data
+        $result = mysqli_query($conn, "UPDATE zodiac SET sign_name='$sign_name',description='$description', daily_horoscope='$daily_horoscope',month_max='$max_month',day_max='$max_day',month_min='$min_month',day_min='$min_day',image_path='$image_path' WHERE id_zodiac=$id");
+        
+        // Redirect to homepage to display updated user in list
+        header("Location: admindashboard.php");
+    }
+?>
+<?php
+// Display selected user data based on id
+// Getting id from url
+$id = $_GET['id'];
+
+// Fetech user data based on id
+$result = mysqli_query($conn, "SELECT * FROM zodiac WHERE id_zodiac=$id");
+
+while($user_data = mysqli_fetch_array($result))
+{
+    $sign_name = $user_data['sign_name'];
+    $description = $user_data['description'];
+    $daily_horoscope = $user_data['daily_horoscope'];
+    $min_month = $user_data['month_min'];
+    $min_day = $user_data['day_min'];
+    $max_month = $user_data['month_max'];
+    $max_day = $user_data['day_max'];
+    $image_path = $user_data['image_path'];
+}
+?>
+<!DOCTYPE html>
+<html>
+	<head>	
+		<title>Edit Zodiac Signs</title>
+		<link rel="stylesheet" href="styles/register.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+        <style>
+            textarea {
+                resize: none;
+            }
+        </style>
+	</head>
+
+	<body>
+		<a href="admindashboard.php" class="btn btn-primary">Home</a>
+		<br/><br/>
+		<?php
+            $months = [
+                1 => "January",
+                2 => "February",
+                3 => "March",
+                4 => "April",
+                5 => "May",
+                6 => "June",
+                7 => "July",
+                8 => "August",
+                9 => "September",
+                10 => "October",
+                11 => "November",
+                12 => "December"
+            ];
+
+            // Function to generate the <option> tags
+            function generateMonthOptions($selectedMonth) {
+                global $months;
+                $options = "<option value='' " . (empty($selectedMonth) ? 'selected' : '') . ">Select Month</option>";
+                foreach ($months as $monthNumber => $monthName) {
+                    $selected = ($monthNumber == $selectedMonth) ? 'selected' : '';
+                    $options .= "<option value='$monthNumber' $selected>$monthName</option>";
+                }
+                return $options;
+            }
+        ?>
+
+		<form name="update_zodiac" method="post" action="edit-zodiac.php">
+            <div class="mb-3">
+                <table width="100%" border="0">
+                    <tr>
+                        <td><label class="form-label">Sign Name</label></td>
+                        <td><input type="text" class="form-control" name="sign_name" value=<?php echo $sign_name;?> required></td>
+                    </tr>
+                    <tr> 
+                        <td><label class="form-label">Description</label></td>
+                        <td><textarea class="form-control" name="description" row=6><?php echo $description;?></textarea></td>
+                    </tr>
+                    <tr> 
+                        <td><label class="form-label">Daily Horoscope</label></td>
+                        <td><textarea name="daily_horoscope" class="form-control"><?php echo $daily_horoscope;?></textarea></td>
+                    </tr>
+                    <tr> 
+                        <td><label for="min_month" class="form-label">First Range Month</label></td>
+                        <td>
+                            <select class="form-select" aria-label="Default select example" name="min_month">
+                                <?php echo generateMonthOptions($min_month); ?>
+                            </select>
+                            <input type="text" id="min_day" class="form-control" name="min_day" maxlength="2" size="2" value=<?php echo $min_day;?> required>
+                            <p id="minErrorMessage" style="color: red; display: none;">Invalid day for the selected month!</p>
+                        </td>
+                    </tr>
+                    <tr> 
+                        <td><label for="max_month" class="form-label">Last Range Month</label></td>
+                        <td><select class="form-select" aria-label="Default select example" name="max_month">
+                                <?php echo generateMonthOptions($max_month); ?>
+                            </select>
+                            <input type="text" id="max_day" class="form-control" name="max_day" maxlength="2" size="2" value=<?php echo $max_day;?> required>
+                            <p id="maxErrorMessage" style="color: red; display: none;">Invalid day for the selected month!</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td><label class="form-label">Image Path</label></td>
+                        <td><input type="text" class="form-control" name="image_path" value=<?php echo $image_path;?> required></td>
+                    </tr>
+                    <tr>
+                        <td><input type="hidden" name="id" value=<?php echo $_GET['id'];?>></td>
+                        <td><input type="submit" class="btn btn-primary"name="update" value="Update"></td>
+                    </tr>
+                </table>
+            </div>
+		</form>
+	</body>
+</html>
+
